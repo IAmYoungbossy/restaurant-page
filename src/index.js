748dd86fba1fff75536b484fc5cb3672eed94821
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-17 18:30:24 +0100

    Link CSS file with index.JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import './style.css';
+
+const body = document.querySelector('body');
+const content = document.querySelector('#content');
+const header = document.createElement('hi');
+
+header.textContent = "Welcome To Our Restaurant";
+
+content.appendChild(header);
+body.appendChild(content);
+
+body.classList.add('hello');
 console.log('Hello Bossman');
