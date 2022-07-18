94cc634de6d3952c0217ec90ceb1d3435ae3b1ad
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-18 10:49:40 +0100

    Create header element and contents with JavaScript

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import './style.css';
 
-const body = document.querySelector('body');
 const content = document.querySelector('#content');
-const header = document.createElement('hi');
+const header = document.createElement('header');
+const logoContainer = document.createElement('div');
+const menuContainer = document.createElement('div');
+const logo = document.createElement('h1');
+const menu = document.createElement('ul');
+const menuHome = document.createElement('li');
+const menuMenu = document.createElement('li');
+const menuContact = document.createElement('li');
 
-header.textContent = "Welcome To Our Restaurant";
+content.appendChild(header);
+header.appendChild(logoContainer);
+header.appendChild(menuContainer);
+logoContainer.appendChild(logo);
+menuContainer.appendChild(menu);
+menu.appendChild(menuHome);
+menu.appendChild(menuMenu);
+menu.appendChild(menuContact);
 
-body.appendChild(header);
-body.appendChild(content);
-
-body.classList.add('hello');
-console.log('Hello Bossman');
+logo.textContent = 'The Burger Palace';
+menuHome.textContent = 'Home';
+menuMenu.textContent = 'Menu';
+menuContact.textContent = 'Contact';
